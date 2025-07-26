9ed508477be5b93aea9ed6074667c38c8f135d99
hannakaczynska <hannakacz13@gmail.com>
2025-07-26 14:52:39 +0200

    add exchange rate fetching functionality in Current component; implement purchase and sale calculations; create store.js for Redux setup; update .gitignore and package.json for new dependencies

--- frontend/src/components/current/current.js
+++ frontend/src/components/current/current.js
@@ -1,7 +1,44 @@
 import css from "./current.module.css";
 import MediaQuery from "react-responsive";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+//set midrates to store in refetch it after 1 hour
+  const fetchExchangeRates = async (setMidEuroRate, setMidGbpRate) => {
+    try {
+      const response = await axios.get("https://api.exchangerate.host/live", {
+        params: {
+          base: "USD",
+          currencies: "EUR, GBP",
+          access_key: process.env.REACT_APP_CURRENCY_API_KEY,
+        },
+      });
+      setMidEuroRate(response.data.quotes.USDEUR);
+      setMidGbpRate(response.data.quotes.USDGBP);
+      console.log("Exchange rates fetched successfully:", response.data.quotes);
+    } catch (error) {
+      console.error("Error fetching exchange rates:", error);
+    }
+  };
 
 const Current = () => {
+  const [midEuroRate, setMidEuroRate] = useState("");
+  const [midGbpRate, setMidGbpRate] = useState("");
+
+  useEffect(() => {
+    fetchExchangeRates(setMidEuroRate, setMidGbpRate);
+  }, []);
+
+  const handlePurchase = (midRate) => {
+    if (!midRate) return "";
+    return (midRate * 1.005).toFixed(3);
+  };
+
+  const handleSale = (midRate) => {
+    if (!midRate) return "";
+    return (midRate * 0.995).toFixed(3);
+  };
+
   return (
     <div className={css.tableContainer}>
       <table className={css.table}>
@@ -13,16 +50,22 @@ const Current = () => {
           </tr>
         </thead>
         <tbody className={css.dataBody}>
-          <tr className={css.dataRow}>
-            <td className={`${css.firstCell} ${css.dataCell}`}>USD</td>
-            <td className={css.dataCell}>36.50</td>
-            <td className={`${css.lastCell} ${css.dataCell}`}>37.20</td>
-          </tr>
-          <tr className={css.dataRow}>
-            <td className={`${css.firstCell} ${css.dataCell}`}>EUR</td>
-            <td className={css.dataCell}>39.80</td>
-            <td className={`${css.lastCell} ${css.dataCell}`}>40.60</td>
-          </tr>
+            <>
+              <tr className={css.dataRow}>
+                <td className={`${css.firstCell} ${css.dataCell}`}>GBP</td>
+                <td className={css.dataCell}>{handlePurchase(midGbpRate)}</td>
+                <td className={`${css.lastCell} ${css.dataCell}`}>
+                  {handleSale(midGbpRate)}
+                </td>
+              </tr>
+              <tr className={css.dataRow}>
+                <td className={`${css.firstCell} ${css.dataCell}`}>EUR</td>
+                <td className={css.dataCell}>{handlePurchase(midEuroRate)}</td>
+                <td className={`${css.lastCell} ${css.dataCell}`}>
+                  {handleSale(midEuroRate)}
+                </td>
+              </tr>
+            </>
         </tbody>
       </table>
       <MediaQuery maxWidth={767}>
@@ -31,7 +74,7 @@ const Current = () => {
       <MediaQuery minWidth={768} maxWidth={1279}>
         <img src="/wave-tablet.svg" alt="Wave" className={css.waveSvg} />
       </MediaQuery>
-            <MediaQuery minWidth={1280}>
+      <MediaQuery minWidth={1280}>
         <img src="/wave-desktop.svg" alt="Wave" className={css.waveSvg} />
       </MediaQuery>
     </div>
