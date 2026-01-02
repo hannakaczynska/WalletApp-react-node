15f851843274107a3da5f3aab7de32aec6a32097
hannakaczynska <hannakacz13@gmail.com>
2026-01-02 09:46:27 +0100

    Creating AppShell component with DesktopNavigation

---
+++ frontend/src/components/AppShell/AppShell.js
@@ -0,0 +1,24 @@
+import DesktopNavigation from "../Navigation/DesktopNavigation";
+import Balance from "../Balance/Balance";
+import Current from "../Current/Current";
+import css from "./AppShell.module.css";
+
+const AppShell = () => {
+  return (
+    <>
+      <div className={css.shellPanel}>
+        <div className={css.container}>
+          <DesktopNavigation />
+          <Balance />
+        </div>
+        <div className={css.current}>
+          <Current />
+        </div>
+      </div>
+      <div className={css.separator}></div>
+      <img src="/ellipse2.svg" alt="Ellipse" className={css.ellipseTwo} />
+      <img src="/ellipse1.svg" alt="Ellipse" className={css.ellipseOne} />
+    </>
+  );
+};
+export default AppShell;
--- frontend/src/pages/dashboard/DashboardPage.js
+++ frontend/src/pages/dashboard/DashboardPage.js
@@ -2,7 +2,9 @@ import css from "./DashboardPage.module.css";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import MediaQuery from "react-responsive";
-import Navigation from "../../components/Navigation/Navigation";
+import MobileNavigation from "../../components/Navigation/MobileNavigation";
+import DesktopNavigation from "../../components/Navigation/DesktopNavigation";
+import AppShell from "../../components/AppShell/AppShell";
 import Current from "../../components/Current/Current";
 import Balance from "../../components/Balance/Balance";
 import CashflowList from "../../components/CashflowList/CashflowList";
@@ -14,7 +16,6 @@ const DashBoardPage = () => {
 
   const [showAddTransaction, setShowAddTransaction] = useState(false);
 
-
   const renderContent = () => {
     switch (location.pathname) {
       case "/home":
@@ -24,7 +25,12 @@ const DashBoardPage = () => {
               <Balance />
             </div>
             <CashflowList />
-            <img src="/add.svg" alt="Add" className={css.addIcon} onClick={() => setShowAddTransaction(true)}/>
+            <img
+              src="/add.svg"
+              alt="Add"
+              className={css.addIcon}
+              onClick={() => setShowAddTransaction(true)}
+            />
             {showAddTransaction && (
               <div className={css.transactionForm}>
                 <TransactionForm
@@ -75,28 +81,11 @@ const DashBoardPage = () => {
     <div className={css.dashboardPage}>
       <div className={css.dashboardContainer}>
         <MediaQuery maxWidth={767.5}>
-          <div className={css.navigation}>
-            <Navigation />
-          </div>
+          <MobileNavigation />
         </MediaQuery>
         <div className={css.dashboard}>
           <MediaQuery minWidth={768}>
-            <div className={css.dashboardLayout}>
-              <div className={css.layoutContainer}>
-                <div className={css.navigation}>
-                  <Navigation />
-                </div>
-                <div className={css.balance}>
-                  <Balance />
-                </div>
-              </div>
-              <div className={css.current}>
-                <Current />
-              </div>
-            </div>
-            <div className={css.separator}></div>
-            <img src="/ellipse2.svg" alt="Ellipse" className={css.ellipseTwo} />
-            <img src="/ellipse1.svg" alt="Ellipse" className={css.ellipseOne} />
+            <AppShell />
           </MediaQuery>
           <div className={css.contentContainer}>
             <div className={css.content}>{renderContent()}</div>
