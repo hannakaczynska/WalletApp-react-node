8dfe2cb1a45bd78649f8020a4492b54cfb605e6b
hannakaczynska <hannakacz13@gmail.com>
2026-01-02 07:57:04 +0100

    changing login page structure

--- frontend/src/pages/login/LoginPage.js
+++ frontend/src/pages/login/LoginPage.js
@@ -1,40 +1,32 @@
 import css from "./LoginPage.module.css";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import MediaQuery from "react-responsive";
 
 const LoginPage = () => {
   return (
-    <div>
-      <div className={css.registrationPage}>
+    <div className={css.loginPage}>
+      <MediaQuery maxWidth={1279}>
+        <LoginForm className={css.loginForm} />
+      </MediaQuery>
+      <h1 className={css.title}>Finance App</h1>
+      <img
+        src="/ellipse2.svg"
+        alt="Ellipse background"
+        className={css.ellipseTwo}
+      />
+      <img src="/frame.svg" alt="Frame background" className={css.frame} />
+      <img
+        src="/ellipse1.svg"
+        alt="Ellipse background"
+        className={css.ellipseOne}
+      />
+      <div className={css.formContainer}>
+        <img
+          src="/rectangle3.png"
+          alt="Rectangle background"
+          className={css.rectangleThree}
+        />
         <LoginForm />
-        <h1 className={css.title}>Finance App</h1>
-        <img
-          src="/ellipse2.svg"
-          alt="Ellipse background"
-          className={css.ellipseTwo}
-        />
-        <img src="/frame.svg" alt="Frame background" className={css.frame} />
-        <img
-          src="/ellipse1.svg"
-          alt="Ellipse background"
-          className={css.ellipseOne}
-        />
-      </div>
-      <div className={css.registrationDesktopPage}>
-        <h1 className={css.title}>Finance App</h1>
-        <img src="/frame.svg" alt="Frame background" className={css.frame} />
-        <img
-          src="/ellipse1.svg"
-          alt="Ellipse background"
-          className={css.ellipseOne}
-        />
-        <div className={css.formContainer}>
-          <img
-            src="/rectangle3.png"
-            alt="Rectangle background"
-            className={css.rectangleThree}
-          />
-          <LoginForm />
-        </div>
       </div>
     </div>
   );
